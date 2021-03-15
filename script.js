96c3b59a41899e9a1159ad2a3d00461cb5e469b4
sachic <scorrear@tutanota.com>
2021-03-15 07:58:04 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Event to eliminate photos from computer science branch selection
+
+document.querySelector('.gallery').addEventListener('click', function(e){
+    console.log(e);
+    e.preventDefault();
+
+    if(e.target.tagName === 'IMG'){
+        var howMany = this.querySelectorAll('li').length;
+
+        if(howMany > 1){
+            var removeTarget = e.target.parentNode;
+            removeTarget.parentNode.removeChild(removeTarget);
+        }
+        else {
+            var photoTitle = e.target.alt;
+            document.querySelector('.computers p').innerHTML = "<p>You've picked " + photoTitle + "</p>"; 
+        }  
+    }
+}, false);
